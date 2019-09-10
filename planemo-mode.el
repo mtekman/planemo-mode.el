b4a8aa51be28382aa81b60d98cd9e3bc2a652f95
Mehmet Tekman <m.tekman@ucl.ac.uk>
2019-09-10 18:52:15 +0200

    skeleton updated

--- planemo-mode.el
+++ planemo-mode.el
@@ -1,21 +1,89 @@
+;;
 
-
+(defun insert-boolean ())
+(defun insert-data ())
+(defun insert-float ())
+(defun insert-input ())
+(defun insert-integer ())
+(defun insert-option ())
+(defun insert-option-nowhen ())
+(defun insert-output ())
+(defun insert-selection ())
+(defun insert-test ())
+(defun insert-test-interactive ())
+(defun insert-text ())
+(defun insert-conditional ())
+(defun insert-param ())
+(defun insert-section ())
+(defun insert-when ())
 
 ;; Minor modes
-(define-minor-mode galaxy-mode)
+(define-minor-mode galaxy-mode
+  "Main galaxy mode"
+  :init-value nil
+  :lighter "Galaxy "
+  :keymap
+  ;; Repeatable elements get modes
+  '(([M-i] . insert-input)
+    ([M-o] . insert-output)
+    ([M-t] . insert-test)))
+
+(define-minor-mode tests-mode
+  "Output mode"
+  :init-value nil
+  :lighter "Tests "
+  :keymap
+  '(([M-t] . insert-test)
+    ([M-i] . insert-test-interactive)))
+
+(define-minor-mode outputs-mode
+  "Output mode"
+  :init-value nil
+  :lighter "Output "
+  :keymap
+  '(([M-d] . insert-data)))
+
+(define-minor-mode inputs-mode
+  "Input mode"
+  :init-value nil
+  :lighter "Input "
+  :keymap
+  '(([M-c] . insert-conditional)
+    ([M-p] . insert-param)
+    ([M-s] . insert-section))
 
 (define-minor-mode conditional-mode
-  "This mode exists in galaxy-mode. Adds a <when value='' /> for every valid param value.")
+  "This mode exists in galaxy-mode. Adds a <when value='' /> for every valid param value."
+  :init-value nil
+  :lighter "Conditional "
+  :keymap
+  '(([M-p] . insert-param)
+    ([M-w] . insert-when)))
 
 (define-minor-mode param-mode
-  "This mode can overlay conditional-mode")
+  "This mode can overlay conditional-mode"
+  :init-value nil
+  :lighter "Param "
+  :keymap
+  '(([M-b] . insert-boolean)
+    ([M-i] . insert-integer)
+    ([M-f] . insert-float)
+    ([M-t] . insert-text)
+    ([M-d] . insert-data)
+    ([M-s] . insert-selection)))
 
-(define-minor-mode selection-mode
-  "This mode can overlay param-mode")
+(define-minor-mode param-selection-mode
+  "This mode can overlay param-mode"
+  :init-value nil
+  :lighter "Selection "
+  :keymap
+  '(([M-O] . insert-option-nowhen)
+    ([M-o] . insert-option)))
+
 
 
 ;; Commands
-(defun macrofy-selection (macroname beg end) 
+(defun macrofy-selection (macroname beg end)
   "Convert selection to a macro and replace selection with expand token. Adds to the <macro/> section. "
   (interactive
    (setq macroname (read-string "macro name (without @) "))))
